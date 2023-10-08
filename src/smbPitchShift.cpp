6b86a9c532efa6e674f0bc66bc9095b1dfdafe35
Maurício <mauriciomoraesnantes@gmail.com>
2023-10-08 13:00:23 -0300

    Fix global value MAX_FRAME_LENGTH to header

--- src/smbPitchShift.cpp
+++ src/smbPitchShift.cpp
@@ -40,8 +40,9 @@
 #include <math.h>
 #include <stdio.h>
 
+#include "smbPitchShift.h"
+
 #define M_PI 3.14159265358979323846
-#define MAX_FRAME_LENGTH 8192
 
 void smbFft(float *fftBuffer, long fftFrameSize, long sign);
 double smbAtan2(double x, double y);
--- src/smbPitchShift.h
+++ src/smbPitchShift.h
@@ -1,6 +1,8 @@
 #ifndef SMBPITCHSHIFT_H // include guard
 #define SMBPITCHSHIFT_H
 
+#define MAX_FRAME_LENGTH 8192
+
 void smbPitchShift(float pitchShift, long numSampsToProcess, long fftFrameSize, long osamp, float sampleRate, float *indata, float *outdata);
 
 #endif
