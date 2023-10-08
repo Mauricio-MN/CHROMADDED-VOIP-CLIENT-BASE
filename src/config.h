5d17dfe40cd58233eeb2c26d75ae397ce23307c6
Maurício <mauriciomoraesnantes@gmail.com>
2023-10-08 12:53:34 -0300

    Add config -> KEY and IV: crpt

--- src/config.h
+++ src/config.h
@@ -8,12 +8,17 @@ class Config{
 
     bool needCryptography;
 
+    bool has_Key;
+    bool has_Iv;
+
     std::vector<char> cryptoKey;
     std::vector<char> cryptoIV;
 
     public:
     Config(){
         needCryptography = true;
+        has_Key = false;
+        has_Iv = false;
     }
 
     void setNeedCryptography(bool isNeed){
@@ -25,19 +30,23 @@ class Config{
     }
 
     std::vector<char> getCryptoKey(){
+        if(!has_Key) perror("Crypto Key not found!");
         return cryptoKey;
     }
 
     void setCryptoKey(std::vector<char> key){
         cryptoKey = key;
+        has_Key = true;
     }
 
     std::vector<char> getCryptoIV(){
+        if(!has_Key) perror("Crypto Iv not found!");
         return cryptoIV;
     }
 
     void setCryptoIV(std::vector<char> iv){
         cryptoIV = iv;
+        has_Iv = true;
     }
 
 };
