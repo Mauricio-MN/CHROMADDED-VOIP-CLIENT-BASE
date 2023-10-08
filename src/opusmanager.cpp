fcebfd3e1ca8921cc75674f6399a7b8e52307b54
Maurício <mauriciomoraesnantes@gmail.com>
2023-10-08 12:54:48 -0300

    Fix opusmanager encode and decode sample rate

--- src/opusmanager.cpp
+++ src/opusmanager.cpp
@@ -23,13 +23,19 @@
         Decerror = 0;
         encoder = opus_encoder_create(SAMPLE_RATE, SAMPLE_CHANNELS, OPUS_APPLICATION_VOIP, &Encerror);
         decoder = opus_decoder_create(SAMPLE_RATE, SAMPLE_CHANNELS, &Decerror);
+        opus_encoder_ctl(encoder, OPUS_SET_BITRATE(OPUS_AUTO));
+        opus_encoder_ctl(encoder, OPUS_SET_BANDWIDTH(OPUS_BANDWIDTH_MEDIUMBAND));
+        opus_encoder_ctl(encoder, OPUS_SET_VBR(1));
+        opus_encoder_ctl(encoder, OPUS_SET_COMPLEXITY(10));
+        opus_encoder_ctl(encoder, OPUS_SET_INBAND_FEC(1));
+        opus_encoder_ctl(encoder, OPUS_SET_PACKET_LOSS_PERC(5));
     }
 
     OpusManager::OpusManager(int sampleRate){
         Encerror = 0;
         Decerror = 0;
-        encoder = opus_encoder_create(SAMPLE_RATE, SAMPLE_CHANNELS, OPUS_APPLICATION_VOIP, &Encerror);
-        decoder = opus_decoder_create(SAMPLE_RATE, SAMPLE_CHANNELS, &Decerror);
+        encoder = opus_encoder_create(sampleRate, SAMPLE_CHANNELS, OPUS_APPLICATION_VOIP, &Encerror);
+        decoder = opus_decoder_create(sampleRate, SAMPLE_CHANNELS, &Decerror);
     }
 
     //Default 640 samples only 16000 sample rate
@@ -56,6 +62,17 @@
         opus_int16* decodedSamples = new opus_int16[sampleCount];
         int decodedLen = opus_decode(decoder, reinterpret_cast<const unsigned char*>(buffer.getData()), buffer.size(), decodedSamples, sampleCount, 0);
 
+        data::buffer outDataBuffer(decodedSamples, decodedLen);
+        delete[] decodedSamples;
+        return outDataBuffer;
+    }
+
+    data::buffer OpusManager::decode(const unsigned char* buffer, int bufferSize, int sampleCount){
+        opus_int16* decodedSamples = new opus_int16[sampleCount];
+        int decodedLen = opus_decode(decoder, buffer, bufferSize, decodedSamples, sampleCount, 0);
+        if(decodedLen < sampleCount){
+            std::cout << "ERR" << std::endl;
+        }
         data::buffer outDataBuffer(decodedSamples, decodedLen);
         delete[] decodedSamples;
         return outDataBuffer;
