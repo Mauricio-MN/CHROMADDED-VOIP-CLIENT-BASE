7aed6b5be5554ddaca49d3a09cdcb9d519d2ac8c
Maurício <mauriciomoraesnantes@gmail.com>
2023-10-08 12:51:13 -0300

    Fixes data Fix Queue's async Add precise sleep

---
+++ src/data.cpp
@@ -0,0 +1,81 @@
+#include "data.h"
+
+namespace data{
+
+    void asyncQueue::objNumCheck(int &num){
+        if(num < 0 || num >= size) num = 0;
+    }
+
+    asyncQueue::asyncQueue(){
+        asyncQueue(256);
+    }
+
+    asyncQueue::asyncQueue(size_t _size): mutexes(_size), objState(_size), obj(_size){
+        size = _size;
+
+        for(int i = 0; i < size; i++){
+            objState[i] = false;
+        }
+
+        writePos = 0;
+        readPos = 0;
+    }
+
+    //thread safe
+    void asyncQueue::push(protocol::Server& objPack){
+        pushMutex.lock();
+
+        int pos = writePos;
+        if(pos >= size) pos -= size;
+
+        mutexes[pos].lock();
+
+            //Escrever sobre
+            obj[pos].CopyFrom(objPack);
+            objState[pos] = true;
+            if(!objState[pos].load()){
+                avaliable++;
+            }
+
+            writePos++;
+            if(writePos.load() >= size) writePos -= size;
+
+        mutexes[pos].unlock();
+
+        pushMutex.unlock();
+    }
+
+    //thread safe
+    bool asyncQueue::pop(protocol::Server& result){
+        popMutex.lock();
+
+        bool readResult = false;
+        int pos = readPos;
+        
+        if(pos >= size) pos -= size;
+
+        if(objState[pos].load()){
+            mutexes[pos].lock();
+
+            if(objState[pos].load()){
+                result.CopyFrom(obj[pos]);
+                objState[pos] = false;
+                readResult = true;
+                avaliable--;
+                readPos++;
+                if(readPos.load() >= size) readPos -= size;
+            }
+            
+            mutexes[pos].unlock();
+        }
+
+        popMutex.unlock();
+
+        return readResult;
+    }
+
+    int asyncQueue::getAvaliable(){
+        return avaliable.load();
+    }
+
+};
--- src/data.h
+++ src/data.h
@@ -1,11 +1,17 @@
 #ifndef DATA_H // include guard
 #define DATA_H
 
+#include <atomic>
 #include <stddef.h>
 #include <string.h>
 #include <vector>
 #include <stdexcept>
 #include <mutex>
+#include <chrono>
+#include <math.h>
+#include <thread>
+#include <shared_mutex>
+#include "proto/protocol.pb.h"
 
 #define ERROR_NO_ERROR        0
 #define ERROR_LOW             10
@@ -21,6 +27,9 @@ struct Coords{
     int z;
     int map;
 };
+
+#define THREAD_POOL_ARGS (int, data::parseThreadPoll&)
+#define THREAD_POOL_ARGS_NAMED (int myid, data::parseThreadPoll& threadPoolL)
 
 namespace data
 {
@@ -65,6 +74,10 @@ namespace data
                 return bufferDataVec.size();
             }
 
+            void resize(int newSize){
+                bufferDataVec.resize(newSize);
+            }
+
             const std::vector<char>& getVector() const{
                 return bufferDataVec;
             }
@@ -77,10 +90,23 @@ namespace data
                 return bufferDataVec.data();
             }
 
+            void writeover(void* dest){
+                memcpy(dest, bufferDataVec.data(), bufferDataVec.size());
+            }
+
             void writeover(int destPos, buffer& Data){
                 if(checkDestPos(destPos)){ return; }
-                else if(Data.size() <= bufferDataVec.size()){
+                if(Data.size() <= bufferDataVec.size()){
                         memcpy(bufferDataVec.data() + destPos, Data.getData(), Data.size());
+                } else {
+                    failSize();
+                }
+            }
+
+            void writeover(int destPos, buffer* Data){
+                if(checkDestPos(destPos)){ return; }
+                if(Data->size() <= bufferDataVec.size()){
+                        memcpy(bufferDataVec.data() + destPos, Data->getData(), Data->size());
                 } else {
                     failSize();
                 }
@@ -89,7 +115,7 @@ namespace data
             void writeover(int destPos, buffer& Data, int size){
                 if(checkValidInsertSize(size)){ return; }
                 if(checkDestPos(destPos)){ return; }
-                else if(destPos + size <= bufferDataVec.size()){
+                if(destPos + size <= bufferDataVec.size()){
                     if(Data.size() <= size){
                         memcpy(bufferDataVec.data() + destPos, Data.getData(), size);
                     } else {
@@ -103,7 +129,7 @@ namespace data
             void writeover(int destPos, char* data, int size){
                 if(checkValidInsertSize(size)){ return; }
                 if(checkDestPos(destPos)){ return; }
-                else if(destPos + size <= bufferDataVec.size()){
+                if(destPos + size <= bufferDataVec.size()){
                     memcpy(bufferDataVec.data() + destPos, data, size);
                 } else {
                     failSize();
@@ -115,16 +141,11 @@ namespace data
                 int size = sizeof(T);
                 if(checkValidInsertSize(size)){ return; }
                 if(checkDestPos(destPos)){ return; }
-                else if(destPos + size <= bufferDataVec.size()){
+                if(destPos + size <= bufferDataVec.size()){
                     memcpy(bufferDataVec.data() + destPos, data, size);
                 } else {
                     failSize();
                 }
-            }
-
-            template <typename T>
-            void writeover(int destPos, T data){
-                writeover(destPos, &data);
             }
 
             void insert(char* data, int size){
@@ -230,105 +251,433 @@ namespace data
 
     private:
         std::vector<std::vector<std::int16_t>> audio;
-        std::vector<bool> audioState;
+        std::vector<std::atomic<bool>> audioState;
         std::vector<std::mutex> mutexes;
         int readPos;
 
+        std::atomic<int> _size;
+
         int checkValues(){
             int result = -1;
-            for(int i = 1; i <= 2; i++){
+            for(int i = 1; i <= 5; i++){
                 int ant = readPos - i;
                 if(ant < 0) ant += 256;
-                if(audioState[ant]){
+                if(audioState[ant].load()){
                     result = ant;
                 } 
             }
             return result;
         }
 
-        void audioNumCheck(int &num){
-            if(num < 0 || num >= 256) num = 0;
-        }
-
-        void init(int sampleCount){
+    public:
+        AudioQueue(): mutexes(256), audioState(256){
+            readPos = 0;
             audio.resize(256);
+
             for(auto& samplePack : audio){
-                samplePack.resize(sampleCount);
+                samplePack.resize(256);
                 for(auto& sample : samplePack){
                     sample = 0;
                 }
             }
 
-            audioState.resize(256);
-
             for(int i = 0; i < 256; i++){
                 audioState[i] = false;
             }
-
-            readPos = 0;
         }
 
-    public:
-        AudioQueue(): mutexes(256){
-            init(1);
-        }
+        void push(int audioNumb, std::vector<std::int16_t>& audioPack){
+            if(audioNumb < 0 || audioNumb >= 256) audioNumb = 0;
 
-        void resize(int sampleCount){
-            for(auto& mutexe : mutexes){
-                mutexe.lock();
-            }
-            if(sampleCount > 0){
-                init(sampleCount);
-            }
-            for(auto& mutexe : mutexes){
-                mutexe.unlock();
+            if(!audioState[audioNumb].load()){
+                if(audioNumb < 0 || audioNumb >= 256) audioNumb = 0;
+                _size++;
+                audio[audioNumb] = audioPack;
+                audioState[audioNumb] = true;
+            } else {
+                mutexes[readPos].lock();
+                audio[audioNumb] = audioPack;
+                audioState[audioNumb] = true;
+                mutexes[readPos].unlock();
             }
         }
 
-        AudioQueue(int sampleCount): mutexes(256){
-            init(sampleCount);
-        }
+        bool pop(std::vector<std::int16_t>& result){
+            if(readPos >= 256) readPos -= 256;
+            bool success = false;
 
-        void push(int audioNumb, std::vector<std::int16_t> audioPack){
-            audioNumCheck(audioNumb);
-            mutexes[audioNumb].lock();
-            audioState[audioNumb] = true;
-            audio[audioNumb] = audioPack;
-            mutexes[audioNumb].unlock();
-        }
+            int checkAnt = checkValues();
+            if(checkAnt > -1){
+                readPos = checkAnt;
+            }
 
-        std::vector<std::int16_t> pop(){
-            std::vector<std::int16_t> result;
-            if(!audioState[readPos]){
-                int checkAnt = checkValues();
-                if(checkAnt > -1){
-                    readPos = checkAnt;
+            if(audioState[readPos].load()){
+                mutexes[readPos].lock();
+                if(audioState[readPos].load()){
+                    result.insert(result.end(), audio[readPos].begin(), audio[readPos].end());
+                    mutexes[readPos].unlock();
+                    success = true;
+                    audioState[readPos] = false;
+                    _size--;
+                } else {
+                    mutexes[readPos].unlock();
                 }
             }
-            mutexes[readPos].lock();
-            if(audioState[readPos]){
-                result = audio[readPos];
-                audioState[readPos] = false;
-            }
-            mutexes[readPos].unlock();
+
             readPos++;
-            return result;
+            if(readPos >= 256) readPos -= 256;
+
+            return success;
+        }
+
+        int size(){
+            return _size.load();
+        }
+
+        int absoluteSize(){
+            int absSize = 0;
+            for(int i = 0; i < 256; i++){
+                if(audioState[i].load()){
+                    absSize++;
+                }
+            }
+            return absSize;
+        }
+
+        int relativeCompleteSize(){
+            int absSize = 0;
+            for(int i = readPos; i < 256; i++){
+                if(audioState[i].load()){
+                    absSize++;
+                } else {
+                    break;
+                }
+            }
+            return absSize;
         }
 
         bool canReadNext(){
-            return audioState[readPos];
+            return audioState[readPos].load();
         }
 
         void hyperSearch(){
             for(int i = 0; i < 256; i++){
-                if(audioState[i]){
+                if(audioState[i].load()){
                     mutexes[i].lock();
                     readPos = i;
                     mutexes[i].unlock();
                 }
             }
         }
+
+        void hyperClean(){
+            for(int i = 0; i < readPos; i++){
+                if(audioState[i].load()){
+                    mutexes[i].lock();
+                    audioState[i] = false;
+                    _size--;
+                    mutexes[i].unlock();
+                }
+            }
+        }
     };
+
+    class asyncQueue{
+
+    private:
+        std::vector<protocol::Server> obj;
+        std::vector<std::atomic<bool>> objState;
+        std::vector<std::mutex> mutexes;
+        std::atomic_int writePos;
+        std::atomic_int readPos;
+        std::atomic_int avaliable;
+        size_t size;
+
+        std::mutex pushMutex;
+        std::mutex popMutex;
+
+        void objNumCheck(int &num);
+
+        void init(size_t _size);
+
+    public:
+        asyncQueue();
+
+        asyncQueue(size_t _size);
+
+        void push(protocol::Server& objPack);
+
+        bool pop(protocol::Server& result);
+
+        int getAvaliable();
+    };
+
+    typedef std::unordered_map<int, std::shared_ptr<asyncQueue>> asyncQueueMap;
+    typedef std::vector<std::shared_ptr<asyncQueue>> asyncQueueVector;
+    typedef std::unordered_map<int, std::shared_ptr<std::thread>> asyncThreadPool;
+    typedef std::unordered_map<int, std::atomic<bool>*> asyncThreadPool_State;
+
+    //thread safe
+    class parseThreadPoll{
+
+        private:
+        asyncQueueMap queue;
+        asyncThreadPool threadPool;
+        asyncThreadPool_State threadState;
+
+        std::shared_mutex syncMutex;
+
+        public:
+
+        void parser(std::function<void THREAD_POOL_ARGS> parse, int myid){
+            syncMutex.lock_shared();
+            bool isRuning = threadState[myid]->load();
+            syncMutex.unlock_shared();
+
+            while(isRuning){
+                parse(myid, *this);
+                
+                syncMutex.lock_shared();
+                bool isRuning = threadState[myid]->load();
+                syncMutex.unlock_shared();
+            }
+        }
+
+        parseThreadPoll(){
+
+        }
+
+        //thread safe
+        void insert(int playerID, std::function<void THREAD_POOL_ARGS> parse){
+            if(!exist(playerID)){
+                syncMutex.lock();
+                threadState[playerID] = new std::atomic<bool>();
+                *threadState[playerID] = true;
+
+                queue[playerID] = std::make_shared<asyncQueue>();
+
+                threadPool[playerID] = std::make_shared<std::thread>([this, parse, playerID]() {
+                    parser(parse, playerID);
+                });
+                
+                syncMutex.unlock();
+            }
+        }
+
+        //thread safe
+        bool exist(int playerID){
+            syncMutex.lock_shared();
+            if(threadPool.find(playerID) != threadPool.end()){
+                syncMutex.unlock_shared();
+                return true;
+            }
+            syncMutex.unlock_shared();
+            return false;
+        }
+
+        //thread safe
+        bool push(int playerID, protocol::Server& received){
+            if(exist(playerID)){
+                syncMutex.lock_shared();
+                queue[playerID]->push(received);
+                syncMutex.unlock_shared();
+                return true;
+            }
+            return false;
+        }
+
+        //thread safe
+        bool pop(int playerID, protocol::Server& result){
+            if(exist(playerID)){
+                syncMutex.lock_shared();
+                bool success = queue[playerID]->pop(result);
+                syncMutex.unlock_shared();
+                return success;
+            }
+            return false;
+        }
+
+        //thread safe
+        void stop(int playerID){
+            if(exist(playerID)){
+                syncMutex.lock();
+                *threadState[playerID] = false;
+                if(threadPool[playerID]->joinable()){
+                    threadPool[playerID]->join();
+                }
+                syncMutex.unlock();
+            }
+        }
+
+        //thread safe
+        void stopAll(){
+
+            syncMutex.lock();
+            for(auto& state : threadState){
+                *state.second = false;
+            }
+            for(auto& threadL : threadPool){
+                if(threadL.second->joinable()){
+                    threadL.second->join();
+                }
+            }
+            threadPool.clear();
+            queue.clear();
+            threadState.clear();
+            syncMutex.unlock();
+
+        }
+
+    };
+
+    /*
+
+    class PlayerBinaryTree{
+
+        private:
+
+        PlayerBinaryTree* left;
+        PlayerBinaryTree* right;
+        PLAYER player;
+        int id;
+
+        bool empty;
+
+        public:
+
+        void setPlayer(PLAYER _player){
+            player = _player;
+        }
+
+        void setId(int _id){
+            id = _id;
+        }
+
+        int leftID(){
+            left->getID();
+        }
+
+        int rightID(){
+            right->getID();
+        }
+
+        int getID(){
+            return id;
+        }
+
+        PLAYER getPlayer(){
+            return player;
+        }
+
+        PlayerBinaryTree(PLAYER _player): player(_player){
+            id = _player->id;
+            empty = false;
+        }
+
+        bool isEmpty(){
+            return empty;
+        }
+
+        void setNotEmpty(){
+            empty = false;
+        }
+
+        int insert(PLAYER& _player, int _id){
+            if(left == nullptr){
+                left = new PlayerBinaryTree(_player);
+            } else if(_id < leftID()){
+                if(left->isEmpty()){
+                    left->setPlayer(_player);
+                    left->setId(_id);
+                    left->setNotEmpty();
+                    return;
+                }
+                left->insert(_player, _id);
+            } else if (right == nullptr){
+                right = new PlayerBinaryTree(_player);
+            } else {
+                if(right->isEmpty()){
+                    right->setPlayer(_player);
+                    right->setId(_id);
+                    right->setNotEmpty();
+                    return;
+                }
+                right->insert(_player, _id);
+            }
+        }
+
+        void remove(int _id){
+            if(_id == id){
+                empty = true;
+                return;
+            }
+
+            if(left != nullptr){
+                if(_id < leftID()){
+                    left->remove(_id);
+                    return;
+                }
+            }
+            if(right != nullptr){
+                if(_id < rightID()){
+                    right->remove(_id);
+                    return;
+                }
+            }
+        }
+
+        PlayerBinaryTree* search(int _id){
+            if(_id == id){
+                return this;
+            }
+
+            if(left != nullptr){
+                if(_id < leftID()){
+                    return left->search(_id);
+                }
+            }
+            if(right != nullptr){
+                return right->search(_id);
+            }
+            return nullptr;
+        }
+
+    };
+    */
+
+    typedef std::chrono::high_resolution_clock clock;
+    template <typename T>
+    using duration = std::chrono::duration<T>;
+
+    static void preciseSleep(double seconds) {
+        using namespace std;
+        using namespace std::chrono;
+
+        static double estimate = 5e-3;
+        static double mean = 5e-3;
+        static double m2 = 0;
+        static int64_t count = 1;
+
+        while (seconds > estimate) {
+            auto start = high_resolution_clock::now();
+            std::this_thread::sleep_for(milliseconds(1));
+            auto end = high_resolution_clock::now();
+
+            double observed = (end - start).count() / 1e9;
+            seconds -= observed;
+
+            ++count;
+            double delta = observed - mean;
+            mean += delta / count;
+            m2   += delta * (observed - mean);
+            double stddev = sqrt(m2 / (count - 1));
+            estimate = mean + stddev;
+        }
+
+        // spin lock
+        auto start = high_resolution_clock::now();
+        while ((high_resolution_clock::now() - start).count() / 1e9 < seconds);
+    }
 
     }
 #endif
