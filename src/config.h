7a8b4d274cd06c8cc58bbc8706116793e3740776
Maurício <mauriciomoraesnantes@gmail.com>
2024-02-18 19:06:51 -0300

    Atomic fix

--- src/config.h
+++ src/config.h
@@ -2,17 +2,22 @@
 #define CONFIG_H
 
 #include "vector"
+#include "atomic"
+#include "mutex"
 
 class Config{
     private:
 
-    bool needCryptography;
+    std::atomic_bool needCryptography;
 
-    bool has_Key;
-    bool has_Iv;
+    std::atomic_bool has_Key;
+    std::atomic_bool has_Iv;
 
     std::vector<char> cryptoKey;
     std::vector<char> cryptoIV;
+
+    std::mutex cryptoKeyMTX;
+    std::mutex cryptoIVMTX;
 
     public:
     Config(){
@@ -26,27 +31,39 @@ class Config{
     }
 
     bool isNeedCryptography(){
-        return needCryptography;
+        return needCryptography.load();
     }
 
     std::vector<char> getCryptoKey(){
-        if(!has_Key) perror("Crypto Key not found!");
-        return cryptoKey;
+        if(!has_Key.load()) perror("Crypto Key not found!");
+        std::vector<char> result;
+        cryptoKeyMTX.lock();
+        result = cryptoKey;
+        cryptoKeyMTX.unlock();
+        return result;
     }
 
     void setCryptoKey(std::vector<char> key){
+        cryptoKeyMTX.lock();
         cryptoKey = key;
         has_Key = true;
+        cryptoKeyMTX.unlock();
     }
 
     std::vector<char> getCryptoIV(){
-        if(!has_Key) perror("Crypto Iv not found!");
-        return cryptoIV;
+        if(!has_Iv.load()) perror("Crypto Iv not found!");
+        std::vector<char> result;
+        cryptoIVMTX.lock();
+        result = cryptoIV;
+        cryptoIVMTX.unlock();
+        return result;
     }
 
     void setCryptoIV(std::vector<char> iv){
+        cryptoIVMTX.lock();
         cryptoIV = iv;
         has_Iv = true;
+        cryptoIVMTX.unlock();
     }
 
 };
